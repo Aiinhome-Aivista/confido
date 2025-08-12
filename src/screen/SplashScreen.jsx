e4d855ff1c3f20f9c6bf9e011a90e198672d654c
subhadip ganguly <skg61988@gmail.com>
2025-08-12 17:51:42 +0530

    loding screen

---
+++ src/screen/SplashScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+SplashScreen
+function SplashScreen() {
+  return (
+    <div>
+      <h1>Hello from SplashScreen!</h1>
+    </div>
+  );
+}
+
+export default SplashScreen;
