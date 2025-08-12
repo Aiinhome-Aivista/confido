2709f053c8b52f4e492177af08c2dccf1f4caaa3
Sanchari-sanchu <sancharikarmakar40@gmail.com>
2025-08-12 18:29:26 +0530

    add route

--- src/common/routes/Index.jsx
+++ src/common/routes/Index.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import ChatScreen from "../../features/screens/ChatScreen"
+import ChooseAvatar from "../../components/select_avtar"
 
 function Index() {
     return (
         <Routes>
             <Route path="chat-screen" element={<ChatScreen />} />
+            <Route path="/select-avatar" element={<ChooseAvatar />} />
         </Routes>
     )
 }
