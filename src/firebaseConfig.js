7606087c8efe38937a6fb2fd3681f0857e7c8e7e
Sayedrakhiya07 <Sayedrakhiya07@gmail.com>
2025-08-25 16:06:29 +0530

    switch firebaseconfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjnW2YH0bqG70xhnSSzdZT5Z5aQtJ5SyY",
-  authDomain: "sign-587c2.firebaseapp.com",
-  projectId: "sign-587c2",
-  storageBucket: "sign-587c2.appspot.com",
-  messagingSenderId: "542340500515",
-  appId: "1:542340500515:web:e895e3eb83d960fb3b3596",
-  measurementId: "G-LC70ZGL5G3"
+  apiKey: "AIzaSyDcNJFFnOOM8KSJzQM8nLsorTZW8S3fBl4",
+  authDomain: "confido-17834.firebaseapp.com",
+  projectId: "confido-17834",
+  storageBucket: "confido-17834.firebasestorage.app",
+  messagingSenderId: "872557856166",
+  appId: "1:872557856166:web:f59c59f786658144c1fb67",
+  measurementId: "G-1KK621VW43"
 };
 
 const app = initializeApp(firebaseConfig);
